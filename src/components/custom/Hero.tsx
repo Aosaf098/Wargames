e978bfb0b33b66287190adbf7eeffe3524b46740
Aosaf098 <aosafibad10@gmail.com>
2025-03-07 00:05:36 +0600

    Marquee Typescript

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -13,6 +13,55 @@ import mcintyre from "../../../public/assets/mcintyre.png";
 import kevin from "../../../public/assets/kevin.png";
 
 const Hero = () => {
+  type SuperstarPhoto = {
+    name: string;
+    photo: any;
+  }[];
+  const topFiveSuperstars: SuperstarPhoto = [
+    {
+      name: "Cody Rhodes",
+      photo: cody,
+    },
+    {
+      name: "Roman Reigns",
+      photo: reigns,
+    },
+    {
+      name: "John Cena",
+      photo: cena,
+    },
+    {
+      name: "CM Punk",
+      photo: punk,
+    },
+    {
+      name: "Randy Orton",
+      photo: randy,
+    },
+  ];
+  const nextFiveSuperstars: SuperstarPhoto = [
+    {
+      name: "Seth Rollins",
+      photo: seth,
+    },
+    {
+      name: "Drew Mcintyre",
+      photo: mcintyre,
+    },
+    {
+      name: "Kevin Owens",
+      photo: kevin,
+    },
+    {
+      name: "Jey Uso",
+      photo: jey,
+    },
+    {
+      name: "AJ Styles",
+      photo: styles,
+    },
+  ];
+
   return (
     <>
       <div className="w-11/12 mx-auto h-[calc(100dvh-110px)] flex items-center justify-center gap-2 p-20">
@@ -31,93 +80,45 @@ const Hero = () => {
           <p className="text-justify w-[85%] text-sm">
             WWE WarGames is the ultimate battleground where superstars collide
             in a chaotic, steel-caged spectacle. For this epic showdown, you can
-            assemble a powerhouse team. Here, two teams clash inside
-            a brutal, roofless steel cage spanning two rings, entering at timed
-            intervals until all warriors are unleashed—only then can victory be
-            claimed by pinfall or submission. This chaotic masterpiece magnifies
-            teamwork and carnage, delivering an electrifying spectacle of
-            unrelenting action. It’s a glorious testament to resilience, where
-            legends are forged in a relentless storm of combat!.
+            assemble a powerhouse team. Here, two teams clash inside a brutal,
+            roofless steel cage spanning two rings, entering at timed intervals
+            until all warriors are unleashed—only then can victory be claimed by
+            pinfall or submission. This chaotic masterpiece magnifies teamwork
+            and carnage, delivering an electrifying spectacle of unrelenting
+            action. It’s a glorious testament to resilience, where legends are
+            forged in a relentless storm of combat!.
           </p>
           <Button className="w-1/4 p-6">Learn More</Button>
         </div>
         <div className="w-1/2 h-full p-6 flex flex-col gap-6 rounded-2xl">
           <div className="w-full h-1/2">
             <Marquee pauseOnHover={true}>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={cody}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={cena}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={reigns}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={punk}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={randy}
-                  alt=""
-                />
-              </div>
+              {topFiveSuperstars.map((superstar, idx) => (
+                <div className="w-72 aspect-square rounded-xl mx-8">
+                  <img
+                    className="w-full h-full object-cover rounded-2xl"
+                    key={idx}
+                    src={superstar.photo}
+                    alt="Top Five Superstars Marquee"
+                  />
+                </div>
+              ))}
+            
             </Marquee>
           </div>
           <div className="w-full h-1/2">
-            <Marquee direction="right">
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={jey}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={styles}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={seth}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={mcintyre}
-                  alt=""
-                />
-              </div>
-              <div className="w-72 aspect-square rounded-xl mx-8">
-                <img
-                  className="w-full h-full object-cover rounded-2xl"
-                  src={kevin}
-                  alt=""
-                />
-              </div>
+          <Marquee direction="right" pauseOnHover={true}>
+              {nextFiveSuperstars.map((superstar, idx) => (
+                <div className="w-72 aspect-square rounded-xl mx-8">
+                  <img
+                    className="w-full h-full object-cover rounded-2xl"
+                    key={idx}
+                    src={superstar.photo}
+                    alt="Next Five Superstars Marquee"
+                  />
+                </div>
+              ))}
+            
             </Marquee>
           </div>
         </div>

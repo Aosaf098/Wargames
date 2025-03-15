5297fb970a7630ebefe5a1832d26922dc908552b
Aosaf098 <aosafibad10@gmail.com>
2025-03-15 11:16:12 +0600

    SelectedRoster length = 0

--- src/components/custom/superstars/Superstars.tsx
+++ src/components/custom/superstars/Superstars.tsx
@@ -5,7 +5,7 @@ import { HomeContext } from "@/Provider/HomeProvider";
 import SelectedRoster from "../Selected/SelectedRoster";
 
 const Superstars = () => {
-  const { superstars } = useContext(HomeContext);
+  const { superstars, selectedRoster } = useContext(HomeContext);
 
   const [isAvailable, setIsAvailable] = useState<boolean>(true);
   const [isSelected, setIsSelected] = useState<boolean>(false);
@@ -25,15 +25,24 @@ const Superstars = () => {
     <>
       <div className="w-3/4 mx-auto">
         <div className="flex items-center justify-center">
-          <span className="text-4xl font-bold bg-red-500 px-8 py-2 rounded-sm">
+          <span className="text-5xl font-bold bg-red-500 px-8 py-2 rounded-sm">
             Superstars
           </span>
         </div>
-        <div className="flex items-center justify-end gap-6 my-8">
+        <div className="flex items-center justify-between gap-6 my-8 px-12">
+          <div>
+            <h1 className="text-4xl font-black italic text-muted-foreground">
+              {
+                isAvailable ? 'Roster' : 'Selected'
+              }
+            </h1>
+          </div>
+          <div className="flex items-center gap-6">
           <Button variant={isAvailable ? 'destructive' : 'default'} onClick={handleisAvailable} className="p-6">
             Available
           </Button>
           <Button variant={isSelected ? 'destructive' : 'default'}  onClick={handleisSelected} className="p-6">Selected</Button>
+          </div>
         </div>
         {isAvailable && (
           <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 justify-items-center">
@@ -43,8 +52,12 @@ const Superstars = () => {
           </div>
         )}
         {
-          isSelected && (
+          (isSelected && selectedRoster.length > 0) ? (
             <SelectedRoster />
+          ) : (
+            <div className="w-11/12 mx-auto mt-24 space-y-4 p-32">
+              <h1 className="text-7xl text-center font-black text-muted-foreground">You Haven't Selected Anyone Yet</h1>
+            </div>
           )
         }
       </div>

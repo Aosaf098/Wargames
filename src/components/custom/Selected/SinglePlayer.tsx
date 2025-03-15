14938bd0544f8790afa71470c8de383baad79729
Aosaf098 <aosafibad10@gmail.com>
2025-03-15 15:57:28 +0600

    MR 2

--- src/components/custom/Selected/SinglePlayer.tsx
+++ src/components/custom/Selected/SinglePlayer.tsx
@@ -29,12 +29,12 @@ const SinglePlayer = ({ singlePlayer }: SuperstarProps) => {
   }
   return (
     <>
-      <div className="h-[200px] flex items-center justify-between gap-4 px-10 bg-accent rounded-xl">
+      <div className="h-[200px] flex items-center justify-between gap-4 lg:px-10 px-4 bg-accent rounded-xl">
         <div className="flex items-center gap-4">
           <img className="lg:w-32 w-28 rounded-lg" src={image} alt="" />
           <div className="space-y-4">
-            <h1 className="text-lg font-bold lg:-mt-14">{name}</h1>
-            <h1 className="italic">{nickname}</h1>
+            <h1 className="md:text-lg text-sm font-bold lg:-mt-14">{name}</h1>
+            <h1 className="italic text-sm md:text-lg">{nickname}</h1>
           </div>
         </div>
         <div>
